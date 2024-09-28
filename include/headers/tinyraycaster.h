bb74618dd5dc1509a22477461d77882ca64adc27
MatteoCalvanico <matteo.calvanico03@gmail.com>
2024-09-28 13:59:48 +0100

    Fix include

--- include/headers/tinyraycaster.h
+++ include/headers/tinyraycaster.h
@@ -1,13 +1,13 @@
 #ifndef TINYRAYCASTER_H
-
 #define TINYRAYCASTER_H
 
 #include <vector>
-#include "../include/headers/map.h"
-#include "../include/headers/player.h"
-#include "../include/headers/sprite.h"
-#include "../include/headers/framebuffer.h"
-#include "../include/headers/textures.h"
+
+#include "map.h"
+#include "player.h"
+#include "sprite.h"
+#include "framebuffer.h"
+#include "textures.h"
 
 struct GameState {
     Map map;
